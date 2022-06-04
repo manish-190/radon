e8ded2a4f5c70940283f39830e969b77893fad8e
manish_kumar <manishmnnit2019@gmail.com>
2022-06-04 16:47:03 +0530

    missing_number

--- src/routes/route.js
+++ src/routes/route.js
@@ -37,216 +37,43 @@ router.get('/candidates/:canidatesName', function(req, res){
     console.log('Candidates name is '+req.params.canidatesName)
     res.send('Done')
 })
-//1st assignment
 
-router.get('/movie1', function(req, res){
-    const movie1 = ["'The Godfather' (1972)","Casablanca' (1943)",
-    "Schindler's List' (1993)","On the Waterfront",
-    "On the Waterfront","The French Connection",
-    "The Silence of the Lambs"]
+//Q1.
+   // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] 
+
+// router.get("/missing", function (req, res) {
+// let arr= [1,2,3,5,6,7]
  
-    movie1.forEach(element =>{console.log(element)
-    });
-    res.send('here is movie list')
-})
+// let total = 0;
+// for (var i in arr) {
+//     total += arr[i];
+// }
+
+// let lastDigit= arr.pop()
+// let consecutiveSum= lastDigit * (lastDigit+1) / 2  //28
+// let missingNumber= consecutiveSum - total
+
+// res.send(  { data: missingNumber  }  );
+// });
 
 
-//2nd assignment
+  // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+router.get("/missing_number", function (req, res) {
+let arr= [33, 34, 35, 37, 38] //150+27 =177
+let len= arr.length
 
-
-router.get('/movies/:indexNumber',function(req,res){
-
-    const movies = ["'The Godfather' (1972)","Casablanca' (1943)",
-"Schindler's List' (1993)","On the Waterfront",
-"On the Waterfront","The French Connection",
-"The Silence of the Lambs"]
-const index = req.params.indexNumber;
-let arr ={}
-  arr[index] = movies[index]
-res.send(arr)
-})
-
-
-
-
-
-
-
-//3rd assignment
-
-router.get('/movies/:indexNumber',function(req,res){
-
-        const movies = ["'The Godfather' (1972)","Casablanca' (1943)",
-    "Schindler's List' (1993)","On the Waterfront",
-    "On the Waterfront","The French Connection",
-    "The Silence of the Lambs"]
-const index = req.params.indexNumber;
-
-if(index < movies.length){
-movie = movies[index];
-}else{
-    movie = ("Uses a valid index")
+let total = 0;
+for (var i in arr) {
+    total += arr[i];
 }
 
- res.send(movie)
-})
+let firstDigit= arr[0]
+let lastDigit= arr.pop()
+let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2 //33+38 =71
+let missingNumber= consecutiveSum - total
 
-
-//4th assignment
-
-
-
-router.get('/films', function (req, res) {
-   const array1 =[ {
-        "id": 1,
-        "name": "The Shining"
-        }, {
-        "id" : 2,
-        "name": "Incendies"
-        }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-        }, {
-        "id": 4,
-        "name": "Finding Nemo"
-        }]
-
-        console.log("Return all array \n")
-    res.send(array1)
+res.send(  { data: missingNumber  }  );
 });
-
-
-
-
-
-
-// router.post('/players',function(req,res){
-//    // let check = req.body.name;
-//     let players =
-//     [
-//         {
-//             "name": "manish",
-//             "dob": "1/1/1995",
-//             "gender": "male",
-//             "city": "jalandhar",
-//             "sports": [
-//                 "swimming"
-//             ]
-//         },
-//         {
-//             "name": "gopal",
-//               "dob": "1/09/1995",
-//             "gender": "male",
-//             "city": "delhi",
-//             "sports": [
-//                 "soccer"
-//             ]
-//         },
-//         {
-//             "name": "lokesh",
-//             "dob": "1/1/1990",
-//             "gender": "male",
-//             "city": "mumbai",
-//             "sports": [
-//                 "soccer"
-//             ]
-//         },
-//     ]
-
-
- 
- 
-//     res.send('all dones')
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //akash sir assignment
-// router.get('assignment1',function(req, res){
-
-//     let arr = [1,2,3,5,6,7]
-    
-//     let total = 0;
-//     for(let i in arr){
-//         total += arr[i];
-//     }
-
-//     let lastDigit = arr.pop()
-
-//     let consecutive_sum = lastDigit *(lastDigit+1) / 2;
-//     let missing_number = consecutive_sum - total
-
-//     res.send({data:missing_number})
-// });
-
-
-//alash sir assignment 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//first assignment
-
-// router.get('/movie1', function(req, res){
-//     const movie1 = ["'The Godfather' (1972)","Casablanca' (1943)",
-//     "Schindler's List' (1993)","On the Waterfront",
-//     "On the Waterfront","The French Connection",
-//     "The Silence of the Lambs"]
- 
-//     movie1.forEach(element =>{console.log(element)
-//     });
-//     res.send('here is movie list')
-// })
-
-// router.get('/id/:ans', function (req, res,) {
-   
-//    // let numbers = numbers
-//     console.log(req.params.id/ans)
-//     const num =[10,20,30,40,50]
-//     console.log(num[numbers]) 
-   
-//     res.send('nuber printing')
-// });
-
-
-
-// router.get('/movie1/:indexNumber', function (req, res) {
-   
-//     const movie1 = ["'The Godfather' (1972)","Casablanca' (1943)",
-//    "Schindler's List' (1993)","On the Waterfront",
-//    "On the Waterfront","The French Connection",
-//    "The Silence of the Lambs"]
-//    let indexNumber = req
-
-//    console.log("movie index number",(movie1[indexNumber]))
-
-//    res.send('please enter movie index number')
-// });
-
 
 module.exports = router;
 // adding this comment for no reason
