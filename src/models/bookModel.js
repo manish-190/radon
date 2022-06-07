bd1a751b9b7cb0bbee8a177b3356848193147d30
manish_kumar <manishmnnit2019@gmail.com>
2022-06-07 21:04:57 +0530

    move on

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,56 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+    // bookName: String, 
+    // authorName: String, 
+    // tags: [String],
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
+    // isPublished: Boolean,
+    // prices: {
+    //     indianPrice: String,
+    //     europePrice: String,
+    // },
+    // sales: {type: Number, default: 10}
+    let 'book', = ({ 
+        name':"Two states",
+        author_id:1,
+        price:50,
+        ratings:4.5,
+    } ,
+
+
+    { 
+        name:"Five Point Someone",
+        author_id:1,
+        price:50,
+        ratings:4.5,
+    } ,
+    { 
+        name:"The 3 Mistakes of My Life",
+        author_id:1,
+        price:50,
+        ratings:4.5,
+    } ,
+    { 
+        name:"One Arranged Murder",
+        author_id:1,
+        price:50,
+        ratings:4.5,
+    } ,
+    { 
+        name:"Harry Porter",
+        author_id:2,
+        price:50,
+        ratings:4.5,
+    } ,
+    { 
+        name:"Harry Porter",
+        author_id:2,
+        price:50,
+        ratings:4.5,
+    } );
+
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model('Book', bookSchema) 
